664116f367e404bb674852aa440781c55f599261
Lovette Duke <qutyluv@gmail.com>
2023-11-28 13:32:56 +0100

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for s in range(97, 123):
-        if chr(s) != 'e' and chr(s) != 'q':
-            print('{}'.format(chr(s)), end="")
+    if chr(s) != 'e' and chr(s) != 'q':
+        print('{}'.format(chr(s)), end="")
