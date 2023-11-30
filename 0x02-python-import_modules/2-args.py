82c37c246dfbbb7a045f0ba1dbc49fcc4c15f626
Lovette Duke <qutyluv@gmail.com>
2023-11-30 17:22:55 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ = '__main__':
+    from sys import argv
 
-n_args = len(argv[1:])
+    n_args = len(argv[1:])
 
-if n_args == 1:
-    print('1 argument:')
-else:
-    print('{:d} arguments{:s}'.format(n_args, '.' if n_args == 0 else ':'))
+    if n_args == 1:
+        print('1 argument:')
+    else:
+        print('{:d} arguments{:s}'.format(n_args, '.' if n_args == 0 else ':'))
 
-for arg in range(1, n_args + 1):
-    print('{:d}: {}'.format(arg, argv[arg]))
+    for arg in range(1, n_args + 1):
+        print('{:d}: {}'.format(arg, argv[arg]))
