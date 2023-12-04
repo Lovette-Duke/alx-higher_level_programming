c56c1d255defc10d807820810d142771f18cd1f1
Lovette Duke <qutyluv@gmail.com>
2023-12-04 17:47:44 +0100

    Task 7 again

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     add = ()
-    if len(tuple_a) < 1:
+    if len(tuple_a) == 0:
         tuple_a = (0, 0)
-    elif len(tuple_b) < 1:
+    elif len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
