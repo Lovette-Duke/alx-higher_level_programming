9f8bed90b43983afcbac89c161f764943bd4aeb4
Lovette Duke <qutyluv@gmail.com>
2023-12-04 17:40:19 +0100

    Task 9 again

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    _max = my_list[0]
     if len(my_list) == 0:
         return None
     else:
+        _max = my_list[0]
         for i, d in enumerate(my_list):
             if d > _max:
                 _max = d
