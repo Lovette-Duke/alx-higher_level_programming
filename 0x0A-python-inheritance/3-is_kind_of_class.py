78e060e1a7ddd038ed786027201b04259f260bc8
Lovette Duke <qutyluv@gmail.com>
2024-01-08 22:27:52 +0100

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This is a module 3-is_kind_of_class.py """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return:
+        True if object is an instance or
+        if object is an instance of a class inherited
+    """
+    return isinstance(obj, a_class)
