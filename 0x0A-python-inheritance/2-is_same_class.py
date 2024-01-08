ecf91fb52b2371e91b0352a12338a8436fc11472
Lovette Duke <qutyluv@gmail.com>
2024-01-08 22:17:51 +0100

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" Return: True if object is exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """ Return: True if object is an instance of the class"""
+    return type(obj) == a_class
