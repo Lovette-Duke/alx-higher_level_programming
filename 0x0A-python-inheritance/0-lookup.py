bb316c59a4c3fb437299c04a74f0682b318406aa
Lovette Duke <qutyluv@gmail.com>
2024-01-08 22:10:11 +0100

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+  Return: lists of attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Return: lists of attributes and methods of an object """
+    return dir(obj)
