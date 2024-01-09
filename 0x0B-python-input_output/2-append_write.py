ef140134212a99be8ff22b2d7f620830a7cc3885
Lovette Duke <qutyluv@gmail.com>
2024-01-09 17:12:38 +0100

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This is module 2-append_write.py """
+
+
+def append_write(filename="", text=""):
+    """ appends to a text file
+        Return: number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
