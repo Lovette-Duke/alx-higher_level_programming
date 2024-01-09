054a94c4badf9e07ac39009ad35c661e001a23cc
Lovette Duke <qutyluv@gmail.com>
2024-01-09 18:38:24 +0100

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is module 3-to_json_string.py """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns JSON representation of a string object """
+
+    return json.dumps(my_obj)
