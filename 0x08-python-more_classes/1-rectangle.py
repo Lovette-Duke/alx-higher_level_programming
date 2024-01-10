316d56bdba248bc42c6d232c62857773653c7b31
Lovette Duke <qutyluv@gmail.com>
2024-01-10 18:46:20 +0100

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This is module 1-rectangle.py """
+
+
+class Rectangle():
+    """ This is a rectangle class """
+    def __init__(self, width=0, height=0):
+        """ initializes the width and height of the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter decorated function for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter decorated function for the width
+            raises error if the width is not an int or if it is negative"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter decorated function for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter decorated function for the height
+            raises error if the height is not an int or if it is negative"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
