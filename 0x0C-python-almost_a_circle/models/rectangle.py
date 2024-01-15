8cf3a7d736d58cc2e6f7d3695a719e20ac1ac59b
Lovette Duke <qutyluv@gmail.com>
2024-01-15 22:33:17 +0100

    indentation fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,4 +93,4 @@ class Rectangle(Base):
     def __str__(self):
         """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height> """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                        self.width, self.height)
+                                                       self.width, self.height)
