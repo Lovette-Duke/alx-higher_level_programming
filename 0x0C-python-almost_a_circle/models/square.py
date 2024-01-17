bf13e4ebad8b7b3a24c7ec2651b72c2fb441a559
Lovette Duke <qutyluv@gmail.com>
2024-01-17 17:52:42 +0100

    Task 10 again

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ initialises the square attributes """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """ returns [Square] (<id>) <x>/<y> - <size> """
