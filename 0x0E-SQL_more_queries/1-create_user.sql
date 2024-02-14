eabb2266acd06207a8db977d8118b23feb84063d
Lovette Duke <qutyluv@gmail.com>
2024-02-14 23:15:40 +0100

    Task 1 again

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates user with all privilages
 
 CREATE USER IF NOT EXISTS 'user_do_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILAGES;
