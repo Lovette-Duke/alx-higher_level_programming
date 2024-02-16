6ca8043e932a2c654da694631798abc99f0d9396
Lovette Duke <qutyluv@gmail.com>
2024-02-16 15:02:26 +0100

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-JOIN tv_shows
-ON tv_genres.show_id = tv_shows.id
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows;
