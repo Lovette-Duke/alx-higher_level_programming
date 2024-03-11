8b22485073098e91c5d3ef0ca39685f22636e8db
Lovette Duke <qutyluv@gmail.com>
2024-03-11 21:08:04 +0100

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const cont = process.argv.length
+
+if (cont <= 2) {
+    console.log('No argument');
+} else if (cont <= 3) {
+    console.log('Argument found');
+} else {
+    console.log("Arguments found")
+}
