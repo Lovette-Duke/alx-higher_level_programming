78d4563a74cbf6e6d059e4177f7689acb4e168c9
Lovette Duke <qutyluv@gmail.com>
2024-03-13 22:12:10 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const Base = require('./5-square');
+
+module.exports = class Square extends Base {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
