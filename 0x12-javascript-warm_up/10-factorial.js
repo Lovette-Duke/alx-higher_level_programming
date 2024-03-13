bc2edb008f8e963ee4959d76e33af4886baf9375
Lovette Duke <qutyluv@gmail.com>
2024-03-13 15:27:08 +0100

    task 10 cc

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factorial (a) {
   if (isNaN(a) || a === 0) {
     return (1);
   }
-  return(a * factorial(a - 1));
+  return (a * factorial(a - 1));
 }
 
 const a = Math.floor(process.argv[2]);
