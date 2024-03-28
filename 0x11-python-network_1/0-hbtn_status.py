fe4f6f49ffa7e00264299eca96c7d786185b1aaa
Lovette Duke <qutyluv@gmail.com>
2024-03-29 00:58:48 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status using urllib
+fetches https://alx-intranet.hbtn.io/status using urllib
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        res = response.read()
-        print('Body response:')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reply:
+        res = reply.read()
+        print('Body reply:')
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
         print("\t- utf8 content: {}".format(res.decode('utf-8')))
