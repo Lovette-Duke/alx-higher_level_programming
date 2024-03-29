e8a1f7b7d86efff2e036b29d2024a6be6090fd22
Lovette Duke <qutyluv@gmail.com>
2024-03-29 01:04:41 +0100

    Task 0 again

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,10 @@ fetches https://alx-intranet.hbtn.io/status using urllib
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reply:
-        res = reply.read()
-        print('Body reply:')
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        res = response.read()
+        print('Body response:')
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
         print("\t- utf8 content: {}".format(res.decode('utf-8')))
