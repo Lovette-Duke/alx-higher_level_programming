2516bd94eaa73415f0b1f8deefba2195a4096a9c
Lovette Duke <qutyluv@gmail.com>
2024-03-29 01:48:00 +0100

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+fetches and displays https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("    - type: {}".format(type(res.text)))
+    print("    - content: {}".format(res.text))
