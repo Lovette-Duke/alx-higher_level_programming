45c88ad5b0f0195b848f97d30bf5f7d6f9c4f797
Lovette Duke <qutyluv@gmail.com>
2024-04-01 21:08:15 +0100

    Task 4 c

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays
 # the body of the response with a header variable.
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id:98" "$1"
